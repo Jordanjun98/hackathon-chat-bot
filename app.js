1284174a65223d1ba47fa58095328a1ac2de2d26
Yu Zhe <kelvinlim_98@live.com>
2017-12-10 19:48:22 +0800

    Add Video

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const connector = new builder.ChatConnector({
 });
 const bot = new builder.UniversalBot(connector).set('storage', inMemoryStorage);
 
+
 // Listen for messages from user
 server.post('/api/messages', connector.listen());
 
@@ -31,7 +32,7 @@ bot.dialog('/', session => {
       session.send('Hello! I am Couch-K!');
       break;
     default:
-			session.beginDialog('userChoice');
+      session.beginDialog('userChoice');
     }
   }
 });
@@ -52,14 +53,43 @@ fs.readFile('data.json', 'utf8', (err, data) => {
 
 // Prompt the choices for the user
 bot.dialog('userChoice', [
-	function (session) {
-		builder.Prompts.choice(session, "How can I help you?", "Continue where you left off|Courses|My Profile", { listStyle: 3 })
-	},
-	function (session, results) {
-		if (results) {
-			console.log(results);
-		} else {
-			session.send('OK');
-		}
-	}
+  function (session) {
+    builder.Prompts.choice(session, 'How can I help you?', 'Continue where you left off|Courses|My Profile', { listStyle: 3 });
+  },
+  function (session, results) {
+    console.log(results);
+    if (results.response.entity === 'Courses') {
+      session.beginDialog('Courses');
+    } else {
+      session.send('OK');
+    }
+  }
 ]);
+
+bot.dialog('Courses', [
+  function (session) {
+    builder.Prompts.choice(session, 'How can I help you?', 'Introduction to Python|Java(To be completed)|More Coming Soon', { listStyle: 3 });
+  },
+  function(session, result){
+    console.log(result);
+    //switch (result){
+    if (result.response.entity === 'Introduction to Python'){
+      session.beginDialog('samplevideo');
+    }//	break;
+    //default:
+    //session.send('Opps, the rest of the courses are under developing xD!');
+    //}
+  }
+]);
+
+bot.dialog('Python1', function (session) {
+  var msg = new builder.Message(session);
+  msg.attachmentLayout(builder.AttachmentLayout.carousel);
+  msg.attachments([
+    new builder.VideoCard(session)
+      .title('Variable')
+      .media([builder.CardMedia.create(session,'https://d3c33hcgiwev3.cloudfront.net/1variables.c86b5453a6afdd5995498484b6f5469a/full/540p/index.mp4?Expires=1513036800&Signature=k4-S1AlgrHX53k4v-G6zgRxBAg26fJDLbfT1R3ukOGP4KPxVOImBM1FWf~uQIcCe3p2kyp5ka9KeP7lLX3sxZz0WzTmfnRX4bntzGw6jj-arnDMTGweynI-gsdububAO9-3iSf44ggzIDPuFgFwCBiXlpSgM87TMh8FPbmjIvR0_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A')]) 
+		
+  ]);	
+  session.send(msg).endDialog();
+});
